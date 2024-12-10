5844dfd3e047992e6557f83bf4d69f4aaa846e5d
JaeAnHan <jaean1999@naver.com>
2024-12-11 03:27:24 +0900

    design: 헤더 배경색 제거

--- frontend/src/layouts/BasicLayout.tsx
+++ frontend/src/layouts/BasicLayout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export function BasicLayout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <header className={"bg-black h-[70px] mb-[30px]"} />
+      <header className={"h-[70px] mb-[30px]"} />
       <div className={"pl-[200px] pr-[200px]"}>{children}</div>
     </div>
   );
