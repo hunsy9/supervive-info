a1010bdd0bab9d451c92c189480899641324ad65
hunsy9 <juniper0917@gmail.com>
2024-12-10 20:51:29 +0900

    feat: 개발 환경 DB 세팅 스크립트 작성

---
+++ backend/init_dev_db.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [[ ! -f manage.py ]]; then
+    echo "No manage.py, wrong location"
+    exit 1
+fi
+
+sleep 1
+
+docker rm -f supervive-postgres-dev
+docker run -it -d -e POSTGRES_DB=supervive -e POSTGRES_USER=supervive -e POSTGRES_PASSWORD=supervive -p 127.0.0.1:5435:5432 --name supervive-postgres-dev postgres:10
